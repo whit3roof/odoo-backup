922cf3c1eeec99cc0169025587ebae54c37fcc49
Pepe <josepp2508@gmail.com>
2026-03-07 09:49:40 -0600

    [feat] db connection func

---
+++ internal/db/connection.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/byt3roof/odoo-backup/internal/conf"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func Connect(ctx context.Context) (*mongo.Client, error) {
+	cfg, err := conf.LoadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	uri := cfg.MongoUri
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	clientOpts := options.Client().
+		ApplyURI(uri).
+		SetServerSelectionTimeout(5 * time.Second)
+
+	client, err := mongo.Connect(clientOpts)
+	if err != nil {
+		return nil, fmt.Errorf("mongo connection error: %w", err)
+	}
+
+	return client, nil
+}
