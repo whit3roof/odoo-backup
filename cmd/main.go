bca7533173e9c835e8d3525c967cef87d3192cb0
Pepe <josepp2508@gmail.com>
2026-03-09 00:48:44 -0600

    [IMP] deletes local .zip file after s3 successful upload

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/byt3roof/odoo-backup/internal/conf"
@@ -59,5 +60,8 @@ func main() {
 			fmt.Printf("failed to upload %s to S3: %v\n", filePath, err)
 			continue
 		}
+
+		_ = os.Remove(filePath)
+		fmt.Printf("successfully backed up and uploaded %s\n", s3Key)
 	}
 }
