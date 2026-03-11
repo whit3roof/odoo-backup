023dded54a844b63477f3e43e9b95dbec37a898f
Pepe <josepp2508@gmail.com>
2026-03-10 21:37:17 -0600

    [FEAT] discord notification on backup succeed & db MB size retrieved and passed in the noti

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 
@@ -53,6 +54,12 @@ func main() {
 		if err != nil {
 			continue
 		}
+		
+		var sizeMB float64
+		if info, err := os.Stat(filePath); err == nil {
+			sizeMB = float64(info.Size()) / (1024 * 1024)
+			fmt.Printf("backup created: %s (%.2f MB)\n", filePath, sizeMB)
+		}
 
 		s3Key := filepath.Base(filePath)
 
@@ -62,6 +69,11 @@ func main() {
 		}
 
 		_ = os.Remove(filePath)
+
+		notiErr := services.SendBackupNotification(cfg, doc["instance"].(string), sizeMB, nil, true)
+		if notiErr != nil {
+			log.Printf("failed to send notification for instance %s", doc["instance"].(string))
+		}
 		fmt.Printf("successfully backed up and uploaded %s\n", s3Key)
 	}
 }
